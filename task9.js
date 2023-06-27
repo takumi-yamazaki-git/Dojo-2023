50b2b84b041f910cf2b33ca67ad9d933c363ef54
takumi-yamazaki-sc-git <135286272+takumi-yamazaki-sc-git@users.noreply.github.com>
2023-06-27 13:12:02 +0900

    file_appload 9 ver3

--- task9.js
+++ task9.js
@@ -1,29 +1,20 @@
 (() => {
   'use strict';
 
-  const eventLs = [
-    'app.record.create.show',
-    'app.record.edit.show',
-  ]
-
   const saveLs = [
     'app.record.create.submit',
     'app.record.edit.submit'
   ]
 
-  kintone.events.on(eventLs, async(event) => {
-    await kintone.app.record.setFieldShown('重複禁止項目', false);
-    return event;
-  });
-
-  kintone.events.on(saveLs, async (saveevent) => {
-    const chValue = saveevent.record.重複禁止項目.value;
-    const param = {app: 32,fields: ['重複禁止項目'], query: '重複禁止項目 = "' + chValue + '"'};
-    const getValuesquery = await kintone.api(kintone.api.url('/k/v1/records.json'), 'GET', param);
-    if (getValuesquery.records.length !== 0) {
+  kintone.events.on(saveLs, async (saveEvent) => {
+    const chValue = saveEvent.record.重複禁止項目.value;
+    const appID = await kintone.app.getId();
+    const param = {app: appID,fields: ['重複禁止項目'], query: '重複禁止項目 = "' + chValue + '"'};
+    const getValues = await kintone.api(kintone.api.url('/k/v1/records.json'), 'GET', param);
+    if (getValues.records.length !== 0) {
       if (window.confirm('レコードが重複しています。このまま保存しますか？') === false) {
-        saveevent = false;
-        return saveevent;
+        saveEvent = false;
+        return saveEvent;
       }
     }
   });
